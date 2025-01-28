f060cd2bccb649fc997e1b770207cfc4bbc8b0ae
Angel <angelmontes7@yahoo.com>
2025-01-28 06:48:52 -0600

    Furthered the project structure and added new features such as nativewind and tailwindcss. Also added new fonts, images and icons.

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+import arrowDown from "@/assets/icons/arrow-down.png";
+import arrowUp from "@/assets/icons/arrow-up.png";
+import backArrow from "@/assets/icons/back-arrow.png";
+import chat from "@/assets/icons/chat.png";
+import checkmark from "@/assets/icons/check.png";
+import close from "@/assets/icons/close.png";
+import dollar from "@/assets/icons/dollar.png";
+import email from "@/assets/icons/email.png";
+import eyecross from "@/assets/icons/eyecross.png";
+import google from "@/assets/icons/google.png";
+import home from "@/assets/icons/home.png";
+import list from "@/assets/icons/list.png";
+import lock from "@/assets/icons/lock.png";
+import map from "@/assets/icons/map.png";
+import out from "@/assets/icons/out.png";
+import person from "@/assets/icons/person.png";
+import pin from "@/assets/icons/pin.png";
+import point from "@/assets/icons/point.png";
+import profile from "@/assets/icons/profile.png";
+import search from "@/assets/icons/search.png";
+import star from "@/assets/icons/star.png";
+import target from "@/assets/icons/target.png";
+import to from "@/assets/icons/to.png";
+import check from "@/assets/images/check.png";
+import message from "@/assets/images/message.png";
+import noResult from "@/assets/images/no-result.png";
+
+export const images = {
+    check,
+    noResult,
+    message,
+};
+
+export const icons = {
+    arrowDown,
+    arrowUp,
+    backArrow,
+    chat,
+    checkmark,
+    close,
+    dollar,
+    email,
+    eyecross,
+    google,
+    home,
+    list,
+    lock,
+    map,
+    out,
+    person,
+    pin,
+    point,
+    profile,
+    search,
+    star,
+    target,
+    to,
+};
+
+export const onboarding = [
+    {
+        id: 1,
+        title: "Page 1",
+        description:
+            "Description",
+        image: null,
+    },
+    {
+        id: 2,
+        title: "Page 2",
+        description:
+            "Description",
+        image: null,
+    },
+    {
+        id: 3,
+        title: "Page 3",
+        description:
+            "Description",
+        image: null,
+    },
+];
+
+export const data = {
+    onboarding,
+};
