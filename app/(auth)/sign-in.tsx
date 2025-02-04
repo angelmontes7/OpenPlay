ba5624e0f7a373ec04fafd5146cc78727b0f9e49
Angel <angelmontes7@yahoo.com>
2025-02-04 11:36:18 -0600

    Sign up and sign in screens with clerk integration for verification of users

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,57 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, View } from "react-native";
-
+import { ScrollView, Text, View, Image } from "react-native";
+import { images, icons } from "@/constants";
+import InputField from "@/components/InputField";
+import { useState } from "react";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+import OAuth from "@/components/OAuth";
 const SignIn = () => {
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    });
+
+    const onSignInPress = async () => {};
+
     return (
-        <SafeAreaView>
-            <Text> Sign In </Text>
-        </SafeAreaView>
+        <ScrollView className="flex-1 bg-white">
+            <View className="flex-1 bg-white">
+                <View>
+                    <Image source={images.onboarding1} className="z-0 w-full h-[250px]" />
+                    <Text className="text-black text-2xl font-JakartaSemiBold left-5">Welcome Back!</Text>
+                </View>
+                <View className="p-5">
+                    <InputField 
+                        label="Email"
+                        placeholderTextColor="#A0A0A0" 
+                        placeholder="Enter your email" 
+                        icon={icons.email} 
+                        value={form.email} 
+                        onChangeText={(value) => setForm({ ...form, email: value })}
+                    />
+                    <InputField 
+                        label="Password"
+                        placeholderTextColor="#A0A0A0" 
+                        placeholder="Enter your password" 
+                        icon={icons.lock} 
+                        secureTextEntry={true}
+                        value={form.password} 
+                        onChangeText={(value) => setForm({ ...form, password: value })}
+                    />
+
+                    <CustomButton title="Log In" onPress={onSignInPress} className="mt-6" />
+
+                    <OAuth />
+
+
+                    <Link href="/(auth)/sign-up" className='text-lg text-center text-general-200 mt-10'>
+                        <Text>Don't have an account? </Text>
+                        <Text className="text-primary-500">Sign Up</Text>
+                    </Link>
+                </View>
+            </View>
+        </ScrollView>
     );
 };
 
-export default SignIn; 
+export default SignIn;
--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -39,7 +39,7 @@ const Onboarding = () => {
                     </View>
                 ))}
             </Swiper>
-            <CustomButton title={isLastSlide ? ' Get Started' : 'Next'} onPress={() => isLastSlide ? router.replace("/(auth)/sign-up") : swiperRef.current ?.scrollBy(1)} className="w-11/12 mt-10" />
+            <CustomButton title={isLastSlide ? 'Get Started' : 'Next'} onPress={() => isLastSlide ? router.replace("/(auth)/sign-up") : swiperRef.current ?.scrollBy(1)} className="w-11/12 mt-10" />
         </SafeAreaView>
     );
 };
