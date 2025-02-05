9e564a3ab62f85963747705ffb5bf3772993f51c
Angel <angelmontes7@yahoo.com>
2025-02-05 14:23:40 -0600

    Allows users to scroll with keyboard if they cant see

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, Text, View, Image } from "react-native";
+import { ScrollView, Text, View, Image, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform } from "react-native";
 import { images, icons } from "@/constants";
 import InputField from "@/components/InputField";
 import { useState } from "react";
@@ -14,43 +14,47 @@ const SignIn = () => {
     const onSignInPress = async () => {};
 
     return (
-        <ScrollView className="flex-1 bg-white">
-            <View className="flex-1 bg-white">
-                <View>
-                    <Image source={images.onboarding1} className="z-0 w-full h-[250px]" />
-                    <Text className="text-black text-2xl font-JakartaSemiBold left-5">Welcome Back!</Text>
-                </View>
-                <View className="p-5">
-                    <InputField 
-                        label="Email"
-                        placeholderTextColor="#A0A0A0" 
-                        placeholder="Enter your email" 
-                        icon={icons.email} 
-                        value={form.email} 
-                        onChangeText={(value) => setForm({ ...form, email: value })}
-                    />
-                    <InputField 
-                        label="Password"
-                        placeholderTextColor="#A0A0A0" 
-                        placeholder="Enter your password" 
-                        icon={icons.lock} 
-                        secureTextEntry={true}
-                        value={form.password} 
-                        onChangeText={(value) => setForm({ ...form, password: value })}
-                    />
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} className="flex-1">
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <ScrollView className="flex-1 bg-white" showsVerticalScrollIndicator={false} keyboardShouldPersistTaps="handled">
+                    <View className="flex-1 bg-white">
+                        <View>
+                            <Image source={images.onboarding1} className="z-0 w-full h-[250px]" />
+                            <Text className="text-black text-2xl font-JakartaSemiBold left-5">Welcome Back!</Text>
+                        </View>
+                        <View className="p-5">
+                            <InputField 
+                                label="Email"
+                                placeholderTextColor="#A0A0A0" 
+                                placeholder="Enter your email" 
+                                icon={icons.email} 
+                                value={form.email} 
+                                onChangeText={(value) => setForm({ ...form, email: value })}
+                            />
+                            <InputField 
+                                label="Password"
+                                placeholderTextColor="#A0A0A0" 
+                                placeholder="Enter your password" 
+                                icon={icons.lock} 
+                                secureTextEntry={true}
+                                value={form.password} 
+                                onChangeText={(value) => setForm({ ...form, password: value })}
+                            />
 
-                    <CustomButton title="Log In" onPress={onSignInPress} className="mt-6" />
+                            <CustomButton title="Log In" onPress={onSignInPress} className="mt-6" />
 
-                    <OAuth />
+                            <OAuth />
 
 
-                    <Link href="/(auth)/sign-up" className='text-lg text-center text-general-200 mt-10'>
-                        <Text>Don't have an account? </Text>
-                        <Text className="text-primary-500">Sign Up</Text>
-                    </Link>
-                </View>
-            </View>
-        </ScrollView>
+                            <Link href="/(auth)/sign-up" className='text-lg text-center text-general-200 mt-10'>
+                                <Text>Don't have an account? </Text>
+                                <Text className="text-primary-500">Sign Up</Text>
+                            </Link>
+                        </View>
+                    </View>
+                </ScrollView>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 };
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, Text, View, Image, Alert, KeyboardAvoidingView } from "react-native";
+import { ScrollView, Text, View, Image, Alert, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform } from "react-native";
 import { images, icons } from "@/constants";
 import InputField from "@/components/InputField";
 import { useState } from "react";
@@ -79,95 +79,99 @@ const SignUp = () => {
     };
 
     return (
-        <ScrollView className="flex-1 bg-white">
-            <View className="flex-1 bg-white">
-                <View>
-                    <Image source={images.onboarding1} className="z-0 w-full h-[250px]" />
-                    <Text className="text-black text-2xl font-JakartaSemiBold left-5">Create Your Account</Text>
-                </View>
-                <View className="p-5">
-                    <InputField 
-                        label="Username"
-                        placeholderTextColor="#A0A0A0"
-                        placeholder="Enter your username" 
-                        icon={icons.person} 
-                        value={form.name} 
-                        onChangeText={(value) => setForm({ ...form, name: value })}
-                    />
-                    <InputField 
-                        label="Email"
-                        placeholderTextColor="#A0A0A0" 
-                        placeholder="Enter your email" 
-                        icon={icons.email} 
-                        value={form.email} 
-                        onChangeText={(value) => setForm({ ...form, email: value })}
-                    />
-                    <InputField 
-                        label="Password"
-                        placeholderTextColor="#A0A0A0" 
-                        placeholder="Enter your password" 
-                        icon={icons.lock} 
-                        secureTextEntry={true}
-                        value={form.password} 
-                        onChangeText={(value) => setForm({ ...form, password: value })}
-                    />
-                    <InputField 
-                        label="DOB"
-                        placeholderTextColor="#A0A0A0"
-                        placeholder="Enter your MM-DD-YYYY" 
-                        icon={icons.person} 
-                        value={form.name} 
-                        onChangeText={(value) => setForm({ ...form, name: value })}
-                    />
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} className="flex-1">
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <ScrollView className="flex-1 bg-white" showsVerticalScrollIndicator={false} keyboardShouldPersistTaps="handled">
+                    <View className="flex-1 bg-white">
+                        <View>
+                            <Image source={images.onboarding1} className="z-0 w-full h-[250px]" />
+                            <Text className="text-black text-2xl font-JakartaSemiBold left-5">Create Your Account</Text>
+                        </View>
+                        <View className="p-5">
+                            <InputField 
+                                label="Username"
+                                placeholderTextColor="#A0A0A0"
+                                placeholder="Enter your username" 
+                                icon={icons.person} 
+                                value={form.name} 
+                                onChangeText={(value) => setForm({ ...form, name: value })}
+                            />
+                            <InputField 
+                                label="Email"
+                                placeholderTextColor="#A0A0A0" 
+                                placeholder="Enter your email" 
+                                icon={icons.email} 
+                                value={form.email} 
+                                onChangeText={(value) => setForm({ ...form, email: value })}
+                            />
+                            <InputField 
+                                label="Password"
+                                placeholderTextColor="#A0A0A0" 
+                                placeholder="Enter your password" 
+                                icon={icons.lock} 
+                                secureTextEntry={true}
+                                value={form.password} 
+                                onChangeText={(value) => setForm({ ...form, password: value })}
+                            />
+                            <InputField 
+                                label="DOB"
+                                placeholderTextColor="#A0A0A0"
+                                placeholder="Enter your MM-DD-YYYY" 
+                                icon={icons.person} 
+                                value={form.name} 
+                                onChangeText={(value) => setForm({ ...form, name: value })}
+                            />
 
-                    <CustomButton title="Sign Up" onPress={onSignUpPress} className="mt-6" />
+                            <CustomButton title="Sign Up" onPress={onSignUpPress} className="mt-6" />
 
-                    <OAuth />
+                            <OAuth />
 
 
-                    <Link href="/sign-in" className='text-lg text-center text-general-200 mt-10'>
-                        <Text>Already have an account? </Text>
-                        <Text className="text-primary-500">Log In</Text>
-                    </Link>
-                </View>
+                            <Link href="/sign-in" className='text-lg text-center text-general-200 mt-10'>
+                                <Text>Already have an account? </Text>
+                                <Text className="text-primary-500">Log In</Text>
+                            </Link>
+                        </View>
 
-                <ReactNativeModal 
-                    isVisible={verification.state === 'pending'}
-                    onModalHide={() => {
-                        if(verification.state === 'success') setShowSuccessModal(true)}}>
-                    <View className='bg-white px-7 py-9 rounded-2xl min-h-[300px]'>
-                        <Text className='text-2xl font-JakartaExtraBold mb-2'>Verification</Text>
-                        <Text className='font-Jakarta mb-5'>We have sent a verification code to {form.email}</Text>
-                        <InputField 
-                            label="Code"
-                            icon={icons.lock}
-                            placeholder="12345"
-                            value={verification.code}
-                            keyboardType="numeric"
-                            onChangeText={(code) => setVerification({ ...verification, code})}
-                        />
-                        {verification.error && (
-                            <Text className="text-red-500 text-sm mt-1">
-                                {verification.error}
-                            </Text>
-                        )}
-                        <CustomButton title="Verify Email" onPress={onVerifyPress} className="mt-5 bg-success-500" />
+                        <ReactNativeModal 
+                            isVisible={verification.state === 'pending'}
+                            onModalHide={() => {
+                                if(verification.state === 'success') setShowSuccessModal(true)}}>
+                            <View className='bg-white px-7 py-9 rounded-2xl min-h-[300px]'>
+                                <Text className='text-2xl font-JakartaExtraBold mb-2'>Verification</Text>
+                                <Text className='font-Jakarta mb-5'>We have sent a verification code to {form.email}</Text>
+                                <InputField 
+                                    label="Code"
+                                    icon={icons.lock}
+                                    placeholder="12345"
+                                    value={verification.code}
+                                    keyboardType="numeric"
+                                    onChangeText={(code) => setVerification({ ...verification, code})}
+                                />
+                                {verification.error && (
+                                    <Text className="text-red-500 text-sm mt-1">
+                                        {verification.error}
+                                    </Text>
+                                )}
+                                <CustomButton title="Verify Email" onPress={onVerifyPress} className="mt-5 bg-success-500" />
+                            </View>
+                        </ReactNativeModal>
+                        
+                        <ReactNativeModal isVisible={showSuccessModal}>
+                            <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+                                <Image source={images.check} className="w-[110px] mx-auto my-5" />
+                                <Text className='text-3xl font-JakartaBold text-center'> Verified</Text>
+                                <Text className='text-base text-gray-400 font-Jakarta text-center mt-2'>You have successfully verified your account.</Text>
+                                <CustomButton 
+                                    title="Browse Home" 
+                                    onPress={() => router.replace('/(root)/(tabs)/home')} 
+                                    className='mt-5' />
+                            </View>
+                        </ReactNativeModal>
                     </View>
-                </ReactNativeModal>
-                
-                <ReactNativeModal isVisible={showSuccessModal}>
-                    <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
-                        <Image source={images.check} className="w-[110px] mx-auto my-5" />
-                        <Text className='text-3xl font-JakartaBold text-center'> Verified</Text>
-                        <Text className='text-base text-gray-400 font-Jakarta text-center mt-2'>You have successfully verified your account.</Text>
-                        <CustomButton 
-                            title="Browse Home" 
-                            onPress={() => router.replace('/(root)/(tabs)/home')} 
-                            className='mt-5' />
-                    </View>
-                </ReactNativeModal>
-            </View>
-        </ScrollView>
+                </ScrollView>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 };
 
