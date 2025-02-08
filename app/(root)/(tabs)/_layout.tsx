338b944048eb30548adc62a328ba21fe2851be8e
Angel <angelmontes7@yahoo.com>
2025-02-08 01:46:06 -0600

    added navbar to naviagate through different tabs when user is logged in. Need new image for wagers filler image for now.

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,13 +1,91 @@
-import { Stack } from 'expo-router';
+import { icons } from "@/constants";
+import { Tabs } from "expo-router";
+import { View, Image, ImageSourcePropType } from "react-native";
 
-const Layout = () => {
-  return (
-    <Stack>
-      <Stack.Screen name="welcome" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
-    </Stack>
-  );
-}
+const TabIcon = ({
+  source,
+  focused,
+}: {
+  source: ImageSourcePropType;
+  focused: boolean;
+}) => (
+  <View
+    className={`flex flex-row w-12 h-12 justify-center items-center rounded-full ${focused ? "bg-general-300" : ""}`}
+  >
+    <View
+      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? "bg-general-400" : ""}`}
+    >
+      <Image
+        source={source}
+        tintColor="white"
+        resizeMode="contain"
+        className="w-7 h-7"
+      />
+    </View>
+  </View>
+);
+
+
+const Layout = () => (
+  <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#333333",
+          borderRadius: 50,
+          paddingBottom: 20, // ios only
+          overflow: "hidden",
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 78,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          position: "absolute",
+        },
+    }}
+  >
+    <Tabs.Screen
+      name="home"
+      options={{
+        title: "Home",
+        tabBarIcon: ({focused}) => (
+          <TabIcon focused={focused} source={icons.home} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="chat"
+      options={{
+        title: "Chat",
+        tabBarIcon: ({focused}) => (
+          <TabIcon focused={focused} source={icons.chat} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="wagers"
+      options={{
+        title: "Wager",
+        tabBarIcon: ({focused}) => (
+          <TabIcon focused={focused} source={icons.wager} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="profile"
+      options={{
+        title: "Profile",
+        tabBarIcon: ({focused}) => (
+          <TabIcon focused={focused} source={icons.profile} />
+        ),
+      }}
+    />
+
+    </Tabs>
+);
 
 export default Layout;
