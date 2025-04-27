6db8879919ed57319befcf154a1d5e7e29b3455f
Angel <angelmontes7@yahoo.com>
2025-04-26 19:15:43 -0500

    Navbar structure (openplay icon needs updating)

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   
   return (
     <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
-      </Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
     </>
   )
 }
