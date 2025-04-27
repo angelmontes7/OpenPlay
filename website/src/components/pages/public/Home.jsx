048fa83288a53694592da1ccd980c0a388751c5f
Angel <angelmontes7@yahoo.com>
2025-04-26 23:24:10 -0500

    Users get directed to dashboard if already logged in

--- website/src/components/pages/public/Home.jsx
+++ website/src/components/pages/public/Home.jsx
@@ -3,8 +3,17 @@ import '../../../App.css';
 import HeroSection from '../../HeroSection';
 import Cards from '../../Cards';
 import Footer from '../../Footer';
+import { useAuth } from '@clerk/clerk-react'; 
+import { Navigate } from 'react-router-dom';
 
 function Home () {
+    const { isSignedIn } = useAuth(); // Get authentication status from Clerk
+
+    // Redirect logic based on whether the user is signed in or not
+    if (isSignedIn) {
+        return <Navigate to="/dashboard" />;
+    }
+
     return (
         <>
             <HeroSection/>
