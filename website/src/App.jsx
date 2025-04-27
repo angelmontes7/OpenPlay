57f4ee3f3eaf21c77fd1210afe55039957d5d719
Angel <angelmontes7@yahoo.com>
2025-04-26 20:51:59 -0500

    .

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import Products from './components/pages/Products';
 
 function App() {
   
@@ -11,6 +14,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/sign-up' element={<SignUp />} />
       </Routes>
     </>
   )
